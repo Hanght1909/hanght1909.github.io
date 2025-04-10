87edec5cfc7fd931951e05deeb53e02551853760
openVietAnh <vietanhtran.uet@gmail.com>
2025-04-10 18:18:23 +0700

    Update basic information

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h1>Hồ Hằng</h1>
+<h2>Urgent:</h2>
+<p>[Hà Nội] Devops/SRE, Mobile, Back-end, BA,...</p>
+<p>[Đà Nẵng] Software Engineer, Data Science Intern/Fresher</p>
+<h2>Contacts:</h2>
+<p>Linked In: <a href="linkedin.com/in/hồ-hằng-3a8416257">Hồ Hằng</a></p>
+<p>Email: hohang1909@gmail.com</p>
