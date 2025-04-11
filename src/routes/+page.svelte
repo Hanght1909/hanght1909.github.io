1aea6fd3a056cb69bb3eee6e763d6b5f5ef2bc99
openVietAnh <vietanhtran.uet@gmail.com>
2025-04-11 17:48:41 +0700

    Add styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,11 @@
-<h1>Hồ Hằng</h1>
-<h2>Urgent:</h2>
-<p>[Hà Nội] Devops/SRE, Mobile, Back-end, BA,...</p>
-<p>[Đà Nẵng] Software Engineer, Data Science Intern/Fresher</p>
-<h2>Contacts:</h2>
-<p>Linked In: <a href="linkedin.com/in/hồ-hằng-3a8416257">Hồ Hằng</a></p>
-<p>Email: hohang1909@gmail.com</p>
+<div class="w-full p-10">
+    <h1 class="font-bold text-2xl">Hồ Hằng</h1>
+    <h2 class="font-bold text-xl">* Urgent:</h2>
+    <p>- [Hà Nội] Devops/SRE, Mobile, Back-end, BA,...</p>
+    <p>- [Đà Nẵng] Software Engineer, Data Science Intern/Fresher</p>
+    <h2 class="font-bold text-xl">* Contacts:</h2>
+    <ul>
+        <li>- Linked In: <a href="linkedin.com/in/hồ-hằng-3a8416257">Hồ Hằng</a></li>
+        <li>- Email: hohang1909@gmail.com</li>
+    </ul>
+</div>
